85a7debed971cf9830c46445bc554cb97b66ff36
isotbiberi <ibaslar@yahoo.com>
2017-09-21 10:32:15 +0300

    ATCL_ACK test

--- ATCL.cpp
+++ ATCL.cpp
@@ -67,7 +67,7 @@ void set_mincount(int fd, int mcount)
 
 int main()
 {
-    char *portname = "/dev/ttyUSB0";
+    char *portname = "/dev/ttyS20";
     int fd;
     int wlen;
 
@@ -77,37 +77,39 @@ int main()
         return -1;
     }
     /*baudrate 115200, 8 bits, no parity, 1 stop bit */
-    set_interface_attribs(fd, B115200);
+    set_interface_attribs(fd, B19200);
     //set_mincount(fd, 0);                /* set to pure timed read */
 
     /* simple output */
-    wlen = write(fd, "Hello!\n", 7);
-    if (wlen != 7) {
+    unsigned char ch = '\xB1';//ATCL_ACK mode
+    wlen = write(fd, &ch , 1);
+    if (wlen != 1) {
         printf("Error from write: %d, %d\n", wlen, errno);
     }
     tcdrain(fd);    /* delay for output */
 
 
     /* simple noncanonical input */
-    do {
+  //  do {
         unsigned char buf[80];
         int rdlen;
 
         rdlen = read(fd, buf, sizeof(buf) - 1);
         if (rdlen > 0) {
-#ifdef DISPLAY_STRING
+//#ifdef DISPLAY_STRING
             buf[rdlen] = 0;
             printf("Read %d: \"%s\"\n", rdlen, buf);
-#else /* display hex */
-            unsigned char   *p;
+//#else /* display hex */
+            unsigned char *p;
             printf("Read %d:", rdlen);
             for (p = buf; rdlen-- > 0; p++)
                 printf(" 0x%x", *p);
             printf("\n");
-#endif
+//#endif
         } else if (rdlen < 0) {
             printf("Error from read: %d: %s\n", rdlen, strerror(errno));
         }
-        /* repeat read to get full message */
-    } while (1);
+    //    /* repeat read to get full message */
+   // } while (1);
+        close(fd);
 }
