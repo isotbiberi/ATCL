b3586ceb771156944330d51e50cb2d0ed94fcd2b
isotbiberi <ibaslar@yahoo.com>
2017-09-27 11:34:10 +0300

    karisti

--- ATCL.cpp
+++ ATCL.cpp
@@ -16,11 +16,11 @@
 #include <iostream>
 #include "ATCLCommands.h"
 #include <bitset>
-
+#include <sstream>
 
 #define thereIsReturn 1
 #define thereIsNoReturn 0
-
+std::string readTillSemicolon(int fd);
 int getReturnAsync(int fd);
 int sendCommand(std::string command,int fd,int length)
 {
@@ -34,6 +34,7 @@ int sendCommand(std::string command,int fd,int length)
 	            return 0;
 }
 bool isSpecial(unsigned char c)
+
 {
 	 std::bitset<8> IntBits=std::bitset<8>(c);
 
@@ -41,36 +42,12 @@ bool isSpecial(unsigned char c)
 	 return isSpecial;
 
 }
-
-bool checkFirstChar(int fd)
-{
-	 unsigned char buf[88];
-	 int rdlen ;
-	 rdlen = read(fd, buf, sizeof(buf) - 1);
-	 if (rdlen > 0) {
-		 if(isSpecial(buf[0]))
-           {
-			 return true;
-           }
-		 else
-		 {
-			 return false;
-		 }
-
-
-	 }
-	 else if (rdlen < 0) {
-		                        printf("Error from read: %d: %s\n", rdlen, strerror(errno));
-		                        exit(0);
-		                    }
-}
-
 std::string getReturnSync(int fd,bool thereReturn)
 {
 	 unsigned char buf[88];
 	 std::string commandReturn="";
 	 int rdlen ;
-	 //bool isAsync = false;
+	 bool isAsync = false;
 	 if(thereReturn==true)
 	 {
 	            do
@@ -78,17 +55,15 @@ std::string getReturnSync(int fd,bool thereReturn)
 
 	                    rdlen = read(fd, buf, sizeof(buf) - 1);
 	                    if (rdlen > 0) {
-                     /*
-	                    if(isSpecial(buf[0]))
+                        if(isSpecial(buf[0]))
                         {
                         	std::cout<<"special character returned "<<std::hex<<(int)buf[0]<<std::endl;
 
                         	getReturnAsync(fd);
                         	isAsync = true;
-
+                            //buf[0]=0;
 
                         }
-                        */
 
 	            //#ifdef DISPLAY_STRING
 	                        buf[rdlen] = 0;
@@ -111,7 +86,7 @@ std::string getReturnSync(int fd,bool thereReturn)
 	             //std::cout<<"appending "<<buf<<std::endl;
 	             commandReturn.append(reinterpret_cast<const char*>(buf));
 	            }
-	            while(buf[rdlen-1]!=';');
+	            while(buf[rdlen-1]!=';'&& !isAsync);
 	            std::cout<<"First char is "<<std::hex<<(int)commandReturn.at(0)<<std::endl;
 	           std::cout<<"Sync return value is " <<commandReturn<<std::endl;
 	           return commandReturn;
@@ -214,9 +189,6 @@ int startATCL(int fd)
 
 }
 
-
-
-
 int set_interface_attribs(int fd, int speed)
 {
     struct termios tty;
@@ -269,6 +241,84 @@ void set_mincount(int fd, int mcount)
 }
 
 
+
+unsigned char readOneChar(int fd)
+{   unsigned char firstChar[1];
+	int rdlen = read(fd, firstChar, 1);
+		if (rdlen > 0) {
+			return firstChar[0];
+
+		}
+		else if (rdlen < 0)
+		{
+		 printf("Error from read: %d: %s\n", rdlen, strerror(errno));
+         exit(0);
+		}
+
+return firstChar[0];
+}
+
+void readAck(int fd)
+{
+	unsigned char firstChar=readOneChar(fd);
+	while(firstChar!=0x8F)
+	{
+		readTillSemicolon(fd);
+		firstChar=readOneChar(fd);
+
+	}
+	std::cout<<"Got ATCL_ACK"<<std::endl;
+
+}
+
+std::string readReturnValue(int fd)
+{
+	unsigned char firstChar=readOneChar(fd);
+	while(isSpecial(firstChar))
+	{
+		readTillSemicolon(fd);
+		firstChar=readOneChar(fd);
+	}
+   std::string returnValue = readTillSemicolon(fd);
+   std::stringstream ss;
+   ss<<firstChar;
+   ss<<returnValue;
+
+   std::cout<<"Return value is " <<ss.str()<<std::endl;
+   return ss.str();
+
+}
+std::string readTillSemicolon(int fd)
+		{
+
+	     unsigned char buf[88];
+		 std::string commandReturn="";
+		 int rdlen ;
+		 do
+		            {
+
+					rdlen = read(fd, buf, sizeof(buf) - 1);
+					if (rdlen > 0) {
+
+
+					buf[rdlen] = 0;
+
+					}
+					else if (rdlen < 0)
+					{
+						printf("Error from read: %d: %s\n", rdlen, strerror(errno));
+
+					}
+
+		             commandReturn.append(reinterpret_cast<const char*>(buf));
+		            }
+		            while(buf[rdlen-1]!=';');
+		 return commandReturn;
+
+		}
+
+
+
 int main()
 {
     const char *portname = "/dev/ttyUSB0";
@@ -293,67 +343,61 @@ sendCommand(AlignFromLastPosition,fd);
 sendCommand(GetAlignmaentState,fd);
 
 */
+/*
 sendCommand(GetRa,fd,6);
-if(checkFirstChar(fd)) getReturnAsync(fd);
-else getReturnSync(fd,thereIsReturn);
-
+getReturnSync(fd,thereIsReturn);
 sendCommand(GetDec,fd,6);
-if(checkFirstChar(fd)) getReturnAsync(fd);
-else getReturnSync(fd,thereIsReturn);
-
+getReturnSync(fd,thereIsReturn);
 sendCommand(GetAlt,fd,6);
-if(checkFirstChar(fd)) getReturnAsync(fd);
-else getReturnSync(fd,thereIsReturn);
-
+getReturnSync(fd,thereIsReturn);
 sendCommand(GetAz,fd,6);
-if(checkFirstChar(fd)) getReturnAsync(fd);
-else getReturnSync(fd,thereIsReturn);
-
+getReturnSync(fd,thereIsReturn);
 
 std::string alt="";
 alt.append(SetTargetAlt,0,5);
 alt.append("+80:00:00;");
 sendCommand(alt,fd,15);
-if(checkFirstChar(fd)) getReturnAsync(fd);
-else getReturnSync(fd,thereIsNoReturn);
+getReturnSync(fd,0);
 
 std::string az="";
 az.append(SetTargetAz,0,5);
 az.append("080:00:00;");
 sendCommand(az,fd,15);
-if(checkFirstChar(fd)) getReturnAsync(fd);
-else getReturnSync(fd,thereIsNoReturn);
+getReturnSync(fd,thereIsNoReturn);
 
 sendCommand(GoToTargetAltAz,fd,6);
-if(checkFirstChar(fd)) getReturnAsync(fd);
-else getReturnSync(fd,thereIsNoReturn);
+getReturnSync(fd,thereIsNoReturn);
 
-std::string progress="0%";
+std::string progress;
 do{
 	sendCommand(GetGoToProgressPercent,fd,6);
-	if(checkFirstChar(fd)) getReturnAsync(fd);
-	else progress=getReturnSync(fd,thereIsReturn);
+	progress=getReturnSync(fd,thereIsReturn);
 	std::cout<<"slewing and progress is "<<progress<<std::endl;
 	sleep(0.5);
   }
 while(progress.compare("100%;")!=0);
 
+//test
+sendCommand(GetRa,fd,6);
+getReturnSync(fd,thereIsReturn);
+sendCommand(GetDec,fd,6);
+getReturnSync(fd,thereIsReturn);
+sendCommand(GetAlt,fd,6);
+getReturnSync(fd,thereIsReturn);
+sendCommand(GetAz,fd,6);
+getReturnSync(fd,thereIsReturn);
+*/
+
 
 sendCommand(GetRa,fd,6);
-if(checkFirstChar(fd)) getReturnAsync(fd);
-else getReturnSync(fd,thereIsReturn);
-
+readReturnValue(fd);
 sendCommand(GetDec,fd,6);
-if(checkFirstChar(fd)) getReturnAsync(fd);
-else getReturnSync(fd,thereIsReturn);
-
+readReturnValue(fd);
 sendCommand(GetAlt,fd,6);
-if(checkFirstChar(fd)) getReturnAsync(fd);
-else getReturnSync(fd,thereIsReturn);
-
+readReturnValue(fd);
 sendCommand(GetAz,fd,6);
-if(checkFirstChar(fd)) getReturnAsync(fd);
-else getReturnSync(fd,thereIsReturn);
+readReturnValue(fd);
+
 
 close(fd);
 }
